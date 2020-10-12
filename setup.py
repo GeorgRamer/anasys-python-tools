262dba6de7817c675e1cc974096a4de2fe881c25
Georg Ramer <georg@ramer.at>
2020-10-12 19:11:14 +0200

    incremented dev version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0dev1',
+    version='0.0.0dev2',
 
     description='A suite of Python tools for use with Anasys Instruments products.',
     long_description=long_description,
